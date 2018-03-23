2e0dcad5899a5ef33e1945403b7d7cce3b8e79d3
tabarul060 <37079540+tabarul060@users.noreply.github.com>
2018-03-23 16:36:19 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+$(document).ready(function(){
+    $(".menu-toggle").click(function(){
+        $('.site-nav').toggleClass('site-nav--open');
+        $(this).toggleClass('open');
+    });
+});
+
+	$(function() {
+	
+		var Page = (function() {
+
+			var $nav = $( '#nav-dots > span' ),
+				slitslider = $( '#slider' ).slitslider( {
+					onBeforeChange : function( slide, pos ) {
+
+						$nav.removeClass( 'nav-dot-current' );
+						$nav.eq( pos ).addClass( 'nav-dot-current' );
+
+					}
+				} ),
+
+				init = function() {
+
+					initEvents();
+					
+				},
+				initEvents = function() {
+
+					$nav.each( function( i ) {
+					
+						$( this ).on( 'click', function( event ) {
+							
+							var $dot = $( this );
+							
+							if( !slitslider.isActive() ) {
+
+								$nav.removeClass( 'nav-dot-current' );
+								$dot.addClass( 'nav-dot-current' );
+							
+							}
+							
+							slitslider.jump( i + 1 );
+							return false;
+						
+						} );
+						
+					} );
+
+				};
+
+				return { init : init };
+
+		})();
+
+		Page.init();
+	
+	});
